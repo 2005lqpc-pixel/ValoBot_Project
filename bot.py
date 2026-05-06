92d1302beeedbf996b0abcf279b4007cc0e9ebc7
2005lqpc-pixel <2005.lqpc@gmail.com>
2026-05-06 16:03:04 +0800

    fix loop name mismatch crash

--- bot.py
+++ bot.py
@@ -16,7 +16,6 @@ class ValorantBot(commands.Bot):
     def __init__(self):
         intents = discord.Intents.default()
         intents.message_content = True
-        intents.members = True
 
         super().__init__(command_prefix="!", intents=intents, help_command=None)
 
